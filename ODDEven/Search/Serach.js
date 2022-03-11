e89c5d1026cfbce68771f15b29121fa7775d471e
Pavithra M <pavit3716@gmail.com>
2022-03-11 17:43:20 +0530

    Assign6

---
+++ ODDEven/Search/Serach.js
@@ -0,0 +1,15 @@
+// function search_ops() {
+//     let input = document.getElementById('searchid').value
+//     input=input.toLowerCase();
+//     let x = document.getElementsByClassName('prod');
+      
+//     for (i = 0; i < x.length; i++) { 
+//         if (!x[i].innerHTML.toLowerCase().includes(input)) {
+//             x[i].style.display="none";
+//         }
+//         else {
+//             x[i].style.display="list-item";                 
+//         }
+//     }
+//   }
+
